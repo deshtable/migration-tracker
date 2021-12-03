22384036c418da759e92e58ce61309b44568afbd
huy260 <91503110+huy260@users.noreply.github.com>
2021-12-03 14:46:21 -0600

    Delete SimpleFramework directory

--- SimpleFramework/feedback.sql
+++
@@ -1,1 +0,0 @@
-select * from source_data except select * from quality_data;
--- SimpleFramework/transformations.sql
+++
@@ -1,3 +0,0 @@
-select concat(bank, branch, lpad(cust, 8, "0"), acnt_num) as account_number,
-cast(interest_rate as decimal(10, 4)) / cast(100 as decimal(10, 4)) as interest_rate,
-date_format(to_date(start_Date, 'yyyyMMdd'), 'yyyy-MM-dd') as date from quality_data;
